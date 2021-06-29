ed74973f5d29b5e2d37cf0b2feff27670c81eb0d
DANIEL AGBAJI <agbajidaniel@hotmail.com>
2021-06-29 16:35:26 -0500

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using DemoMVC4EF.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoMVC4EF.Data
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> TblRestaurant { get; set; }
+    }
+}
---
+++ Entities/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoMVC4EF.Entities
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string RestuarantName { get; set; }
+        public string RestuarantLocation { get; set; }
+
+    }
+}
